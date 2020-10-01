4232cb0569e201e24777a518fd7cc0a0026b3384
Kirat <hrakhra@mail.uoguelph.ca>
2020-09-30 20:14:26 -0400

    created make bucket, and login

--- awsS3Shell.py
+++ awsS3Shell.py
@@ -1,11 +1,62 @@
 import boto3
+from botocore.config import Config
+from configparser import ConfigParser
+from botocore.exceptions import ClientError
 
+key = ""
+secret_key = ""
+client = ""
+region = ""
+session = None
+session_tok = ""
+s3 = None
+
+def login():
+  global key, secret_key, client, region, session, session_tok, s3
+  config = ConfigParser()
+  config.read('config.ini')
+  # print(config['DEFAULT']['AccessKey'])
+  key = config['DEFAULT']['AccessKey']
+  secret_key = config['DEFAULT']['SecretKey']
+  region = config['DEFAULT']['Region']
+  session_tok = config['DEFAULT']['aws_session_token']
+  
+  try:
+    session = boto3.Session(
+      aws_access_key_id=key,
+      aws_secret_access_key=secret_key,
+      aws_session_token=session_tok
+    )
+  except ClientError as error:
+    raise error
+  
+  print("Creating Session....")
+
+  try:
+    s3 = session.resource('s3')
+  except ClientError as error:
+    raise error
+  
+def mkbucket():
+  scan = input("$ Enter name for Bucket: ")
+  print("Creating Bucket....")
+  try:
+    bucket = s3.create_bucket(
+      Bucket = scan,
+    )
+  except ClientError as error:
+    raise error
+  
 
 def run_shell():
   while True:
     command = input("$ ")
     if command == "exit":
       break
+    elif(command == "login"):
+      login()
+    elif(command == "mkbucket"):
+      mkbucket()
     else:
        print(command)
 
