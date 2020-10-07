5ba1b6a78f019c00288c099fb6c5fc051531c573
Kirat <hrakhra@mail.uoguelph.ca>
2020-10-06 22:54:40 -0400

    create table function added, load table function added

--- loadTable.py
+++ loadTable.py
@@ -4,8 +4,37 @@ import csv
 file_name = ""
 table_name = ""
 
+def load_table(table):
+  global file_name, table_name
+
+  if("." not in file_name):
+    print("Invalid File Name")
+    return
+
+  # dynamodb = boto3.resource('dynamodb')
+  # table = dynamodb.Table(table_name)
+  with open(file_name) as csvfile:
+    reader = csv.reader(csvfile,delimiter=',')
+  
+    for row in reader:
+      row.append(row[1]+"/"+row[2])
+      table.put_item(
+        Item = {
+          'commodity':row[0],
+          'variable':row[1],
+          'year':row[2],
+          'units':row[3],
+          'mfactor':row[4],
+          'value':row[5],
+          'variableyear':row[6]
+        }
+      )
+
 def create_table(dynamodb=None):
   global file_name, table_name
+  if(len(file_name) == 0 or len(table_name) == 0):
+    print("Invalid File Name")
+    return
 
   if not dynamodb:
     dynamodb = boto3.resource('dynamodb')
@@ -28,42 +57,26 @@ def create_table(dynamodb=None):
         'AttributeType':'S'
       },
       {
-        'AttributeName':'variable',
-        'AttributeType':'S'
-      },
-      {
-        'AttributeName':'year',
-        'AttributeType':'S'
-      },
-      {
         'AttributeName':'variableyear',
         'AttributeType':'S'
       },
-      {
-        'AttributeName':'units',
-        'AttributeType':'S'
-      },
-      {
-        'AttributeName':'mfactor',
-        'AttributeType':'S'
-      },
-      {
-        'AttributeName':'value',
-        'AttributeType':'N'
-      }
-    ]
+    ],
+    ProvisionedThroughput={
+      'ReadCapacityUnits': 10,
+      'WriteCapacityUnits': 10
+    }
   )
+  return table
   
 def get_name():
   global file_name, table_name
   file_name = input("Enter File Name:")
-  table_name = input("Enter Table Name: ")
-  
+  table_name = input("Enter Table Name: ")  
+
 get_name()
-if(len(file_name) == 0 or len(table_name) == 0):
-  print("Invalid File Name")
-  return
-
-create_table()
-
+table = create_table()
+print("Waiting for table creation...")
+table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
+print("Table created loading file...")
+load_table(table)
 
